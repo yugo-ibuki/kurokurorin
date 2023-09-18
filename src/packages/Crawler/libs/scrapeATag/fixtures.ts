1711f2bc0e2f677ded0b4539377afcebf546a24f
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-18 15:59:19 +0900

    test: scrapeATag

---
+++ src/packages/Crawler/libs/scrapeATag/fixtures.ts
@@ -0,0 +1,8 @@
+export const fixtures = `
+  <div>
+    <a href="https://example.com">example.com</a>
+    <a href="https://example.com/1">example.com/1</a>
+    <a href="https://example.com/2">example.com/2</a>
+    <a href="https://example2.com/1">example2.com/1</a>
+  </div>
+`
---
+++ src/packages/Crawler/libs/scrapeATag/scrapeATag.test.ts
@@ -0,0 +1,36 @@
+import { fixtures } from './fixtures.ts'
+import { scrapeATag } from './scrapeATag.ts'
+import { afterAll, beforeAll, expect } from 'vitest'
+import type { Page, Browser } from 'puppeteer'
+import { launch } from 'puppeteer'
+import { BrowserConfig } from '@config/BrowserConfig.ts'
+
+describe('scrapeATag test', function () {
+  let browser: Browser
+  let page: Page
+
+  beforeAll(async () => {
+    browser = await launch(BrowserConfig)
+    page = await browser.newPage()
+  })
+
+  afterAll(async () => {
+    await page.close()
+    await browser.close()
+  })
+
+  test('Expected Behavior', async () => {
+    // const browser = new Browser()
+    // const page = await browser.newPage()
+    await page.setContent(fixtures)
+    const result = await scrapeATag(page)
+    console.log(result)
+    expect(result).toEqual([
+      'https://example.com/',
+      'https://example.com/1',
+      'https://example.com/2',
+      'https://example2.com/1'
+    ])
+    expect(1).toBe(1)
+  })
+})
