847618884961ff9eaf08ac9825958f5a8d4799ab
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-18 23:48:19 +0900

    chore: use local chrome

--- src/config/BrowserConfig.ts
+++ src/config/BrowserConfig.ts
@@ -2,5 +2,7 @@ import type { PuppeteerLaunchOptions } from 'puppeteer'
 
 export const BrowserConfig: PuppeteerLaunchOptions = {
   headless: 'new',
+  executablePath:
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   args: ['--no-sandbox', '--disable-setuid-sandbox', '--incognito']
 }
