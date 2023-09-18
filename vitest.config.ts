4fde1dd5a5d14a285921d5740d49a5b38329eb89
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-18 15:59:05 +0900

    build: vitest config for alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import * as path from 'path'
 
 export default defineConfig({
   test: {
     globals: true
+  },
+  resolve: {
+    alias: {
+      '@config': path.join(__dirname, 'src/config')
+    }
   }
 })
