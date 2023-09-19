af63472dfeda864189c51a86501cfc9e11f46512
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-20 00:53:17 +0900

    feat: create active crawl base

--- src/packages/Crawler/activeCrawl/activeCrawl.ts
+++ src/packages/Crawler/activeCrawl/activeCrawl.ts
@@ -1,6 +1,62 @@
-import type { CrawlOptionsType } from '@config/CrawlOptions'
+import type { Page as PuppeteerPage } from 'puppeteer'
+import { onlyAllowedDomain } from '@packages/Crawler/libs/onlyAllowedDomein'
+import { concatArraysAndWillBeUnique } from '@utils/concatArraysAndWillBeUnique'
+import { deepCrawl } from '@packages/Crawler/libs/deepCrawl'
 
-export const activeCrawl = async (options: CrawlOptionsType) => {
-  console.log(options)
-  console.log('here is activeCrawl')
+type ActiveCrawlOptions = {
+  startTime: Date
+  userOptions: {
+    crawlingTime: number
+    allowedDomain: string
+  }
+}
+
+/**
+ * 複数のタグに対し、動作を行い url を収集し、再帰的にクローリングしていく。
+ * @param page - puppeteer の page オブジェクト
+ * @param visitedUrls - 既に訪れた URL の配列
+ * @param options - クローリングに必要なオプション
+ * @param no - 何番目の URL か
+ * @returns - 重複排除された URL の配列
+ */
+export const activeCrawl = async (
+  page: PuppeteerPage,
+  visitedUrls: string[] = [],
+  options: ActiveCrawlOptions,
+  no: number = 0
+): Promise<string[]> => {
+  console.log('activeCrawl is called times: ', no)
+
+  const { startTime } = options
+  const { crawlingTime, allowedDomain } = options.userOptions
+
+  // クローリング指定時間を経過したら終了
+  const isTimeUp = Date.now() - startTime.getTime() >= crawlingTime * 1000
+  if (isTimeUp) {
+    return visitedUrls
+  }
+
+  console.log('will scrape a tag')
+  const urls = await deepCrawl(page, options)
+
+  // ドメイン制限
+  const onlyAllowedDomainUrl = await onlyAllowedDomain(urls, allowedDomain)
+
+  // マージして重複排除
+  const uniqueVisitedUrls = concatArraysAndWillBeUnique(
+    visitedUrls,
+    onlyAllowedDomainUrl
+  )
+
+  console.log('will go to next page: ', uniqueVisitedUrls[no])
+
+  // 一つずつページに遷移していく
+  await page.goto(uniqueVisitedUrls[no], {
+    waitUntil: ['load', 'networkidle2']
+  })
+
+  const resultUrls = await activeCrawl(page, uniqueVisitedUrls, options, no + 1)
+  console.log(resultUrls)
+
+  return []
 }
