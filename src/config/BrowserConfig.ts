118078ff84f0b476cb780b39af7a8e50b7fc5400
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-29 17:43:23 +0900

    chore: add headless flag

--- src/config/BrowserConfig.ts
+++ src/config/BrowserConfig.ts
@@ -1,7 +1,9 @@
 import type { PuppeteerLaunchOptions } from 'puppeteer'
 
+const isHeadless = true
+
 export const BrowserConfig: PuppeteerLaunchOptions = {
-  headless: 'new',
+  headless: isHeadless ? 'new' : false,
   executablePath:
     '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   args: ['--no-sandbox', '--disable-setuid-sandbox', '--incognito']
