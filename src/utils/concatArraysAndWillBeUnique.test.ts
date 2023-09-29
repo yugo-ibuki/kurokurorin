5bea1234750fda66ebbde67b271c8a1ecf181cc3
yugo-ibuki <y.ibuki91@gmail.com>
2023-09-29 15:36:20 +0900

    chore: to eng

--- src/utils/concatArraysAndWillBeUnique.test.ts
+++ src/utils/concatArraysAndWillBeUnique.test.ts
@@ -2,7 +2,7 @@ import { describe, test } from 'vitest'
 import { concatArraysAndWillBeUnique } from './concatArraysAndWillBeUnique'
 
 describe('concatUniqueArray', () => {
-  test('重複が排除されていること', () => {
+  test('duplicates should be removed', () => {
     const array1 = ['a', 'b', 'c', 'd', 'e']
     const array2 = ['c', 'd', 'e', 'f', 'g']
     const expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
